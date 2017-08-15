37e1efcf8c425448f050723234e56abd7d42762d
dale-smith <dale.smith.workapp01@gmail.com>
2017-08-15 12:19:01 +0200

    Player and Field Created.
    Created the Player and Field Object, began working on the script. New Assets have been imported.

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+	}
+
+    //Called just before nay physics calculations this is where physics will go
+    void FixedUpdate() {
+
+    }
+}
