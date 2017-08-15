22bde69047e64c2cc0310d7fe8ef565b07ca07c5
dale-smith <dale.smith.workapp01@gmail.com>
2017-08-15 16:23:49 +0200

    Exit Button Added
    Added an Exit Button for the Application. Allowing the User to Exit after Winning.

---
+++ Roll a Ball/Assets/Scripts/ExitController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ExitController : MonoBehaviour {
+    
+    public void ExitClick()
+    {
+        Application.Quit();
+    }
+
+}
