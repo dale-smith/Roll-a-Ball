b67c7f76301bf43ebce5c6a277e3efc80f1a548b
dale-smith <dale.smith.workapp01@gmail.com>
2017-08-15 15:03:30 +0200

    Added Walls, Tokens and Physics.
    Added Walls, Tokens, Physics and Colors to Objects. Player can now collide with Tokens and bounce off the Walls.

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    //Public Variables
+    public GameObject player;
+
+    //Private Variables
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+
+    // Update is called once per frame
+    // LateUpdate is used for Follow Cameras, Proceedional Animation and Gathering Last Known States.
+    void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+
+    
+}
--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -3,20 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    // Public Variables - Public allows you to make changes to this variable in the Unity Editor.
+    public float speed; 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+    //Private Variables
+    private Rigidbody rb; // Accessing the Rigidbody FUnction in the Unity Inspector Dashboard of the object.
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
 	}
 
     //Called just before nay physics calculations this is where physics will go
     void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
+        //Vector3 holds 3x/three decimal values in one container Vector3 (x, y, z)
+        Vector3 movement = new Vector3(moveHorizontal,0, moveVertical); // x = Horizontal, y = top/up, z = Vertical
+        rb.AddForce (movement * speed);
+    }
+
+    /** Destroy - It destroys the game object from the scene, thus causing it lose its value.
+        Destroy(other.gameObject);**/
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Token"))
+        {
+            // Disables the Game Object, allowing it to retain its value.
+            other.gameObject.SetActive(false);
+        }
     }
 }
